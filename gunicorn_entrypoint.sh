0ed65ea1bfc2d6ca85a6a8668ae5eaabda57d096
mohsinali <mohsinali530121@gmail.com>
2025-06-09 22:51:30 +0530

    add migration in entry point

--- gunicorn_entrypoint.sh
+++ gunicorn_entrypoint.sh
@@ -2,6 +2,6 @@
 # Entrypoint for running Gunicorn with Uvicorn workers
 
 # Run database migrations here if needed, e.g.:
-# alembic upgrade head
+alembic upgrade head
 
 exec gunicorn main:app -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000 --timeout 120
